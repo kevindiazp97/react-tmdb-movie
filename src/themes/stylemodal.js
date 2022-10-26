e1dc35f54816d4fff2e237beae71f3a836061fab
kevindiazp97 <kevindiazp97@gmail.com>
2022-10-26 23:07:40 +0700

    Add style modal

---
+++ src/themes/stylemodal.js
@@ -0,0 +1,12 @@
+export const modalStyles = {
+    wrapper: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 300,
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+        p: 4,
+    }
+};
